f80d6cf67644f7062d6a95e97c543cd9716a1bde
Agus <aagiribar@yahoo.es>
2023-12-18 14:58:26 +0000

    Added little improvements

--- src/main/java/software/ulpgc/kata4/Track.java
+++ src/main/java/software/ulpgc/kata4/Track.java
@@ -1,5 +1,11 @@
 package software.ulpgc.kata4;
 
-public record Track(String title, String albumTitle, String genre, String composer, int milliseconds, int bytes, double unitPrice) {
-
+public record Track (
+        String title,
+        String albumTitle,
+        String genre,
+        String composer,
+        int milliseconds,
+        int bytes,
+        double unitPrice) {
 }
